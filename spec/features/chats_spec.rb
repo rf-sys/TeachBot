1ae705814d63daebadaae35aa2d9673aaa957fa6
Okalia <rodionfedorov2016@gmail.com>
2017-02-10 20:45:38 +0200

    Fix travis config

--- spec/features/chats_spec.rb
+++ spec/features/chats_spec.rb
@@ -7,21 +7,18 @@ describe 'Chats', :type => :feature, js: true do
     page.set_rack_session(:user_id => @initiator.id)
   end
 
-  it 'creates conversation' do
+  it 'creates chat' do
     visit conversations_path
-
     expect(page).to have_content('Chat')
-
     fill_in 'Username...', with: @recipient.username
     page.has_css?('table.table')
     expect(page).to have_button('Send message')
     click_button('Send message')
+    sleep(1)
     expect(page).to have_content('New message to ' + @recipient.username)
     message = 'My first chat with you'
     fill_in 'Type your message', with: message
-
     find('#modalNewMessage').find_button('Send message').click
-
     expect(page).to have_content("#{@initiator.username}: #{message} (a few seconds ago)")
   end
 end
