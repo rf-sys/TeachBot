4fa608c1df0225b6b2cf56483b41b9fe7d6b7650
Okalia <rodionfedorov2016@gmail.com>
2017-03-12 16:12:40 +0200

    Fix bug when we render empty div if poster is not presented

--- app/assets/javascripts/components/GlobalSearch/GlobalSearchCourse.jsx
+++ app/assets/javascripts/components/GlobalSearch/GlobalSearchCourse.jsx
@@ -5,7 +5,15 @@ class GlobalSearchCourse extends React.Component {
 
     renderPoster() {
         let poster = this.props.course.poster;
-        return poster ? <img src={poster}/> : null
+
+        if (poster)
+            return (
+                <div className="col-md-4">
+                    <img src={poster}/>
+                </div>
+            );
+        else
+            return null;
     }
 
     render() {
@@ -14,10 +22,8 @@ class GlobalSearchCourse extends React.Component {
             <a href={`/courses/${course.slug}`}
                className="round-0 list-group-item list-group-item-action flex-column align-items-start">
                 <div className="row align-items-center">
-                    <div className="col-md-4">
-                        {this.renderPoster()}
-                    </div>
-                    <div className="col-md-8">
+                    {this.renderPoster()}
+                    <div className="col">
                         <h5 className="mb-1">{course.title}</h5>
                         <small>{moment(course.updated_at).format('LLL')}</small>
                         <p className="mb-1">{course.description}</p>
