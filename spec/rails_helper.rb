a45f38409a4141533f8b029c3cd0171ae9b7c54c
Okalia <rodionfedorov2016@gmail.com>
2017-03-29 16:58:37 +0300

    Fix bug when we used development environment in tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,8 @@ require 'rspec/rails'
 require 'helpers'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+# set test env variables
+Dotenv.overload('.env.test')
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
