a77921289ce736059056e7238bef064fdccc0ddc
Okalia <rodionfedorov2016@gmail.com>
2017-04-19 19:45:39 +0300

    Improve sidekiq configs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,7 @@
+Sidekiq.configure_client do |config|
+  config.redis = { size: 1 }
+end
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { size: 5 }
 end
