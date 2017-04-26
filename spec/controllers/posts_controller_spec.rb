bfe121182586c3a0513f8c38af502ff9a29794b8
Okalia <rodionfedorov2016@gmail.com>
2017-04-26 19:31:42 +0300

    Fix tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,27 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-  describe 'POST #attachment' do
-    render_views
-
-    before(:each) do
-      auth_as(create(:user))
-      set_json_request
-    end
-
-    it 'raises error if url is invalid' do
-      post :attachment, params: { url: 'http://asdafgds.com/' }
-      expect(response).to have_http_status(403)
-      expect(response.body).to match(/Resource is invalid or unavailable/)
-    end
-
-    it 'returns attachment if everything is ok' do
-      post :attachment, params: { url: 'https://google.com/' }
-      expect(response).to have_http_status(200)
-      expect(response.content_type).to eq('application/json')
-    end
-  end
-
   describe 'POST #create' do
     before(:each) do
       auth_as(create(:user))
