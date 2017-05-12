3cba7cb1245b4de9651c47792a3138ebac17e524
Okalia <rodionfedorov2016@gmail.com>
2017-05-12 20:34:28 +0300

    Make a components status list

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -1,1 +1,20 @@
-//= require_tree ./components
+// require each components separately to know which of them has been rewritten and which are not yet.
+
+//= require_tree ./components/Attachments
+//= require_tree ./components/Blocks
+//= require_tree ./components/Chat
+//= require_tree ./components/ChatParticipantsEditor
+//= require_tree ./components/Chats
+//= require_tree ./components/CourseParticipantsBlock
+//= require_tree ./components/global
+// require_tree ./components/GlobalSearch - rewritten
+// require_tree ./components/Notifications - rewritten
+//= require_tree ./components/Paginator
+//= require_tree ./components/ShareLessonButtons
+//= require_tree ./components/StatusAlert
+//= require_tree ./components/SubscribersCounter
+//= require_tree ./components/SubscribeToCourseBtn
+//= require_tree ./components/Subscriptions
+//= require_tree ./components/UnreadMessages
+//= require_tree ./components/UserCourses
+//= require_tree ./components/UserPosts
